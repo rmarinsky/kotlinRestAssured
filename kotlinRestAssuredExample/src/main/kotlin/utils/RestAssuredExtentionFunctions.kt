60a8229db5a99a752a68cb1278de048c76c4ae34
rmarinsky <bravoaqa@gmail.com>
2017-10-02 00:52:12 +0300

    remove useless extension function

--- kotlinRestAssuredExample/src/main/kotlin/utils/RestAssuredExtentionFunctions.kt
+++ kotlinRestAssuredExample/src/main/kotlin/utils/RestAssuredExtentionFunctions.kt
@@ -1,6 +1,5 @@
 package utils
 
-import io.restassured.response.Response
 import io.restassured.response.ResponseBodyExtractionOptions
 import io.restassured.specification.RequestSpecification
 
@@ -8,10 +7,6 @@ fun RequestSpecification.When(): RequestSpecification {
     return this.`when`()
 }
 
-inline fun <reified T> Response.As(): T {
-    return this.`as`(T::class.java)
-}
-
-inline fun <reified T> ResponseBodyExtractionOptions.As(): T {
+inline fun <reified T> ResponseBodyExtractionOptions.As(): T? {
     return this.`as`(T::class.java)
 }
